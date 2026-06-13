112e9fc243e11741071526792f4b3ae27ee88534
Leonardo <leodionisiolima@gmail.com>
2026-06-13 00:30:09 -0300

    xbtDaemon: snapshot 2026-06-13 00:30:09 -03

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -24,10 +24,10 @@ end)
 return {
   window_close_confirmation = "AlwaysPrompt",
   font = wezterm.font("Reddit Mono", { weight = "Regular" }),
-  font_size = 9,
+  font_size = 12,
   adjust_window_size_when_changing_font_size = false,
   enable_tab_bar = true,
-  use_fancy_tab_bar = true,
+  use_fancy_tab_bar = false,
   hide_tab_bar_if_only_one_tab = false,
   tab_bar_at_bottom = false,
   tab_max_width = 25,
