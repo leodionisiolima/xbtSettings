6823f80c278f0b0769f44fb5d83fa85b2bdb6d8c
Leonardo <leodionisiolima@gmail.com>
2026-06-21 18:16:09 -0300

    xbtDaemon: snapshot 2026-06-21 18:16:09 -03

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -45,9 +45,9 @@ return {
     bottom = 0,
   },
 
-  -- +15% more opaque (0.45 -> 0.52): the general window/compositor transparency
+  -- +10% more opaque (0.52 -> 0.57): the general window/compositor transparency
   -- washes the terminal out more than intended, so bump opacity to darken it back.
-  window_background_opacity = 0.52,
+  window_background_opacity = 0.57,
 
   default_cursor_style = "SteadyUnderline",
   cursor_blink_ease_in = "Constant",
